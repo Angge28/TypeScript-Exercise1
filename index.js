018cb1ff4f8a7706d642028ea65fc2b0efb363a1
Angge28 <109935586+Angge28@users.noreply.github.com>
2022-08-09 18:44:46 +0800

    Update exercise 1

--- index.js
+++ index.js
@@ -1,71 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.displayCities = void 0;
-let nameOfCity = document.getElementById("name_of_city");
-let country = document.getElementById("country");
-let population = document.getElementById("population");
-let searchText = document.getElementById("searchText");
-let saveBtn = document.getElementById("submitBtn");
-let myList = document.getElementById("myList");
-let searchBtn = document.getElementById("searchBtn");
-let cityInfoArray = [];
-let searchResultArray = [];
-const addCityInfo = (data) => {
-    cityInfoArray.push(data);
+exports.displayInfo = exports.saveInfo = void 0;
+var cityList = [];
+const saveInfo = (info) => {
+    cityList.push(info);
+    (0, exports.displayInfo)();
+    localStorage.setItem("Data", JSON.stringify(cityList));
 };
-const displayCities = (data) => {
-    if (myList) {
-        while (myList.hasChildNodes() && myList.firstChild) {
-            myList.removeChild(myList.firstChild);
+exports.saveInfo = saveInfo;
+const displayInfo = () => {
+    let list = document.getElementById("my_list");
+    if (list) {
+        while (list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild);
         }
     }
-    data.forEach((item) => {
-        let cityLi = document.createElement("li");
-        let countryLi = document.createElement("li");
-        let populationLi = document.createElement("li");
-        let br = document.createElement("br");
-        cityLi.innerText = "City: " + item.city;
-        countryLi.innerText = "Country: " + item.country;
-        populationLi.innerText = "Population " + item.population;
-        if (myList) {
-            myList.appendChild(cityLi);
-            myList.appendChild(countryLi);
-            myList.appendChild(populationLi);
-            myList.appendChild(br);
+    cityList.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        if (list) {
+            list.appendChild(li);
         }
     });
 };
-exports.displayCities = displayCities;
-saveBtn === null || saveBtn === void 0 ? void 0 : saveBtn.addEventListener('onClick', () => {
-    var newCity = {
-        city: nameOfCity.value,
-        country: country.value,
-        population: population.value
-    };
-    addCityInfo(newCity);
-    console.log(cityInfoArray);
-    nameOfCity.value = "";
-    country.value = "";
-    population.value = "";
-    (0, exports.displayCities)(cityInfoArray);
-});
-searchBtn === null || searchBtn === void 0 ? void 0 : searchBtn.addEventListener('onClick', () => {
-    searchResultArray = cityInfoArray.filter((data) => {
-        if ((data.city.toLocaleLowerCase()).includes(searchText.value)) {
-            return data;
-        }
-        else if ((data.country.toLocaleLowerCase()).includes(searchText.value)) {
-            return data;
-        }
-        else {
-            const message = document.createElement("li");
-            message.innerText = " Result not found";
-            if (myList) {
-                myList === null || myList === void 0 ? void 0 : myList.appendChild(message);
-            }
-        }
-    });
-    console.log(searchResultArray);
-    (0, exports.displayCities)(searchResultArray);
-    searchText.value = "";
-});
+exports.displayInfo = displayInfo;
--- index.ts
+++ index.ts
@@ -1,88 +1,24 @@
-let nameOfCity = (document.getElementById("name_of_city") as HTMLInputElement);
-let country = (document.getElementById("country") as HTMLInputElement);
-let population = (document.getElementById("population") as HTMLInputElement);
-let searchText = (document.getElementById("searchText") as HTMLInputElement);
-let saveBtn = document.getElementById("submitBtn")
-let myList = document.getElementById("myList")
-let searchBtn = document.getElementById("searchBtn")
+var cityList: any[] = [];
 
-interface InfoOfCity {
-    city: string,
-    country: string,
-    population: string
+export const saveInfo = (info: any[]) => {
+    cityList.push(info);
+    displayInfo();
+    localStorage.setItem("Data", JSON.stringify(cityList));
 }
 
-let cityInfoArray:InfoOfCity[] = [];
-let searchResultArray:InfoOfCity[] = [];
-
-const addCityInfo = (data: InfoOfCity) =>{
-        cityInfoArray.push(data)
-    }
-
-export const displayCities = (data:InfoOfCity[])=>{
-   if(myList) {
-    while(myList.hasChildNodes() && myList.firstChild) {
-        myList.removeChild(myList.firstChild);
-    }
-}
-
-data.forEach((item:InfoOfCity) =>{
-    let cityLi = (document.createElement("li") as HTMLLIElement)
-    let countryLi = document.createElement("li")
-    let populationLi = document.createElement("li")
-    let br = document.createElement("br")
-
-    cityLi.innerText = "City: " + item.city
-    countryLi.innerText = "Country: " + item.country
-    populationLi.innerText = "Population " + item.population
-    if(myList){
-        myList.appendChild(cityLi)
-        myList.appendChild(countryLi)
-        myList.appendChild(populationLi)
-        myList.appendChild(br)
-    }
-})
-}
-
-saveBtn?.addEventListener('onClick', () => {
-    var newCity: InfoOfCity = {
-        city:  nameOfCity.value,
-        country: country.value,
-        population: population.value
-     }
-     
-     addCityInfo(newCity);
-     console.log(cityInfoArray)
-
-     nameOfCity.value = ""
-     country.value = ""
-     population.value = ""
-
-     displayCities(cityInfoArray);
-
-})
-
-searchBtn?.addEventListener('onClick', () => {
-     searchResultArray = cityInfoArray.filter((data: InfoOfCity) => {
-           
-        if((data.city.toLocaleLowerCase()).includes(searchText.value)) {
-            return data
-           }
-           else if((data.country.toLocaleLowerCase()).includes(searchText.value)){
-            return data
-           }
-           else{
-            const message = document.createElement("li")
-            message.innerText = " Result not found"
-            if(myList){
-                myList?.appendChild(message)
-            }
+export const displayInfo = () => {
+    let list = document.getElementById("my_list");
+    if(list) {
+        while(list.hasChildNodes() && list.firstChild){
+            list.removeChild(list.firstChild);
         }
-           
-    } )
-   
-    console.log(searchResultArray);
-    displayCities(searchResultArray)
-    searchText.value = ""
+    }
+    cityList.forEach((item) => {
+        let li = document.createElement("li")
+        li.innerText = item;
+        if(list){
+            list.appendChild(li);
+             
+        }
+    })
 }
-)
